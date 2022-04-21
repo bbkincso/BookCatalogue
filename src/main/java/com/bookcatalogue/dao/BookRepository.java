0d978fcfd677b6cf905f9d51c2dcf9bf123dceeb
bbkincso <bbkincso@gmail.com>
2022-04-21 13:59:27 +0100

    Adding micrpservice to this repository

---
+++ src/main/java/com/bookcatalogue/dao/BookRepository.java
@@ -0,0 +1,16 @@
+package com.bookcatalogue.dao;
+
+import com.bookcatalogue.dto.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthor(String author);
+    List<Book> findByTitle(String title);
+    List<Book> findByIsbn(String isbn);
+}
